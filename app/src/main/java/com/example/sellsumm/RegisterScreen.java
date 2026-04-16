5ad79259704a92f5c17d3672a30d4f5ce9527fa0
N1116021 <oluwanifise.obabire2022@my.ntu.ac.uk>
2026-04-16 01:21:32 +0100

    Register Screen authentication begins

--- app/src/main/java/com/example/sellsumm/RegisterScreen.java
+++ app/src/main/java/com/example/sellsumm/RegisterScreen.java
@@ -12,11 +12,13 @@ public class RegisterScreen extends AppCompatActivity
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
